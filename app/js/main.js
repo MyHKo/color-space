5baa3e80af2cc2974b6f2dc1b9e62a26d54116a5
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-07-23 18:20:23 +0200

    script: make the main script

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,31 @@
 'use strict'
 import {setInitialPositions} from "./style.js"
+import {generateBlack, generateRed, generateBlue, generateYellow} from "./colors.js";
 
+function changeColor(circle){
+    const color = circle.classList[1]
+
+    switch(color){
+        case "black":
+            generateBlack(circle)
+            break;
+        case "red":
+            generateRed(circle)
+            break;
+        case "blue":
+            generateBlue(circle)
+            break;
+        case "yellow":
+            generateYellow(circle)
+            break;
+    }
+}
 setInitialPositions();
 
+document.addEventListener('click', (event) => {
+    if(event.target.classList.contains('circle')){
+        changeColor(event.target)
+    }
+})
 
 
